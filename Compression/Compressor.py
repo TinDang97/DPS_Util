157e68804cc4e4c87a2e135c82cd879ce792c018
tindang <rainstone1029x@gmail.com>
2019-12-31 17:23:16 +0700

    -- upload code

---
+++ Compression/Compressor.py
@@ -0,0 +1,32 @@
+import json
+import numpy
+import lzma
+import pickle
+
+SUPPORT_TYPE = [numpy.ndarray, dict, bytes, str, list, int, float]
+
+
+def compress(data, compress_lv=lzma.PRESET_EXTREME, check=lzma.CHECK_SHA256):
+    """
+    if ndarray => using numpy tobytes, otherwise using json.dumps -> encode("utf-8")
+    :param check:
+    :param compress_lv:
+    :param data:
+    :return:
+    """
+    assert type(data) in SUPPORT_TYPE
+    if isinstance(data, numpy.ndarray):
+        data = pickle.dumps(data, pickle.HIGHEST_PROTOCOL)
+    if not isinstance(data, bytes):
+        data = json.dumps(data).encode("utf-8")
+    compressed = lzma.compress(data, preset=compress_lv, check=check, format=lzma.FORMAT_XZ)
+    return compressed
+
+
+def decompress(binary):
+    data = lzma.decompress(binary, format=lzma.FORMAT_XZ)
+    try:
+        data = json.loads(data)
+    except json.JSONDecodeError:
+        data = pickle.loads(data)
+    return data
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # DPS_Util
 This repository contain all util like compression, opencv2 toolkit,...
+
+## Todo:
+- ~~Add compressor~~
+- Add opencv toolkit
+
+## Task Done:
+
+### Compression Lossless:
+- Support type: ndnumpy, dict, string, float, int
+- Compress by lzma
+- if data's type is ndnumpy, compressor will dump by Pickle (support only Python)
+otherwise json
