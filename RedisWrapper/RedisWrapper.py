844b81ba5389c45c3ea3f4d031b16c8e64f5e38c
tindang <rainstone1029x@gmail.com>
2020-02-04 13:14:00 +0700

    --upload code

--- RedisWrapper/RedisWrapper.py
+++ RedisWrapper/RedisWrapper.py
@@ -1,6 +1,7 @@
 from redis import Redis
 
 
-def initial_redis(host='localhost', port=6379, db=0, password=""):
+def initial_redis(host='localhost:6379', db=0, password=""):
+    host, port = host.split(":")
     db = Redis(host=host, port=port, db=db, password=password)
     return db
