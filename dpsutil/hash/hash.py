bb573814cdc13de22f9cd07dbc40412b1968cb4e
tindang <rainstone1029x@gmail.com>
2020-02-10 17:31:23 +0700

    -- upload code

--- dpsutil/hash/hash.py
+++ dpsutil/hash/hash.py
@@ -2,10 +2,12 @@ import hashlib
 import datetime
 
 
-def short_hash(msg):
-    return hashlib.sha1(msg.encode("UTF-8")).hexdigest()[:10]
+def short_hash(msg: (str, bytes)) -> str:
+    assert isinstance(msg, (str, bytes))
+    if type(msg) is str:
+        msg = msg.encode("UTF-8")
+    return hashlib.sha1(msg).hexdigest()[:10]
 
-def hash_now():
+
+def hash_now() -> str:
     return short_hash(datetime.datetime.now().isoformat())
-
-
