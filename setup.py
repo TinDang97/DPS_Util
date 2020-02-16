5ef9e23a7e4394d6b3f8d7b874e74dcc55d38dae
tindang <rainstone1029x@gmail.com>
2020-02-16 21:54:58 +0700

    -- upload code

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(name='dpsutil',
       license='MIT',
       packages=find_packages(),
       install_requires=[
+            'scikit-build'
             'numpy',
             'redis',
             'kafka-python',
