c361c949161a1f399b7ae3288e408dd1228007cb
tindang <rainstone1029x@gmail.com>
2020-02-20 12:53:00 +0700

    -- upload code

--- dpsutil/distances/function.py
+++ dpsutil/distances/function.py
@@ -1,6 +1,11 @@
 import numpy
 
 
+def normalize_L2(x):
+    assert isinstance(x, numpy.ndarray)
+    return x / numpy.sqrt((x ** 2).sum(keepdims=True, axis=1))
+
+
 def cosine_similarity(a, b):
     assert type(a) is numpy.ndarray or type(b) is numpy.ndarray
     return numpy.dot(a, b) / (numpy.linalg.norm(a) * numpy.linalg.norm(b))
