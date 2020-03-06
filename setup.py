7665a4dc88a2fbd961dfd30172b9fa3b57c7d38c
tindang <rainstone1029x@gmail.com>
2020-03-06 14:19:00 +0700

    optimize performance pool.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='dpsutil',
-      version='0.1',
-      description='This repository contain all util like compression, redis, kafka (with custom config),'
-                  ' logging_tool',
+      version='1.0.0',
+      description='This repository contain all util',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/TinDang97/DPS_Util',
       author='TinDang',
       author_email='rainstone1029x@gmail.com',
-      license='MIT',
       packages=find_packages(),
       install_requires=[
             'scikit-build',
@@ -20,4 +24,9 @@ setup(name='dpsutil',
             'ffmpeg-python',
             'natsort'
       ],
-      zip_safe=False)
+      python_requires='>=3.6',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ])
