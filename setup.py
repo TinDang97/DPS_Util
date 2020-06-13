bf5c9a21475f9220e836028b2b82d9c113e7f7e1
tindang <rainstone1029x@gmail.com>
2020-06-13 11:44:10 +0700

    -- release v1.3.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='dpsutil',
-      version='1.3.4',
+      version='1.3.5',
       description='This repository contain all util',
       long_description=long_description,
       long_description_content_type="text/markdown",
